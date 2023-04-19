e8ef24bf7b3629e819849895bdede1c097131fce
Arntit Zonia <arditzonia@gmail.com>
2023-04-19 21:37:28 +0100

    built createMessageWithTimestamp helper function

---
+++ server/src/helpers/messages.ts
@@ -0,0 +1,16 @@
+import moment from "moment";
+
+/**
+ * createMessageWithTimestamp takes an object with any number of properties,
+ * adds a formatted timestamp to it as the 'createdAt' property and returns the resulting object.
+ *
+ * @param args - An object with any number of properties.
+ * @returns - The input object with an additional 'createdAt' property containing a formatted timestamp.
+ */
+const createMessageWithTimestamp = (args: { [key: string]: any }) => {
+  const formattedTimestamp = moment(new Date().getTime()).format("HH:mm:ss DD-MM-YYYY");
+
+  return { ...args, createdAt: formattedTimestamp };
+};
+
+export default createMessageWithTimestamp;
