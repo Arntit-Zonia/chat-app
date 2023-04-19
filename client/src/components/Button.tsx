da47b1c6394841e793f400a8ec2cfc2df57f1e50
Arntit Zonia <arditzonia@gmail.com>
2023-04-19 21:36:09 +0100

    built Button component

---
+++ client/src/components/Button.tsx
@@ -0,0 +1,36 @@
+import { FC } from "react";
+
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  background-color: #0077b6;
+  color: #fff;
+  font-size: 14px;
+  font-weight: 500;
+  border: none;
+  border-radius: 4px;
+  padding: 10px;
+  cursor: pointer;
+  transition: background-color 0.3s;
+
+  &:disabled {
+    background-color: #ccc;
+    cursor: not-allowed;
+  }
+
+  &:not(:disabled):hover {
+    background-color: #014d82;
+  }
+`;
+
+interface IButton {
+  children: string;
+  disabled?: boolean;
+  onClick?: () => void;
+}
+
+const Button: FC<IButton> = ({ children, ...props }) => {
+  return <StyledButton {...props}>{children}</StyledButton>;
+};
+
+export default Button;
