88c2dad463fd72f5339b2a21c98442146166aa59
Arntit Zonia <arditzonia@gmail.com>
2023-04-21 22:24:23 +0100

    Applied react router to the App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,29 @@
 import { FC } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import { createGlobalStyle } from "styled-components";
 
 import Chat from "./components/Chat";
 
 import SocketContext, { socket } from "./SocketContext";
+import Join from "./components/Join";
 
-const App: FC = () => {
-  return (
-    <SocketContext.Provider value={socket}>
-      <Chat />
-    </SocketContext.Provider>
-  );
-};
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #111b21;
+  }
+`;
+
+const App: FC = () => (
+  <SocketContext.Provider value={socket}>
+    <GlobalStyle />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Join />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
+  </SocketContext.Provider>
+);
 
 export default App;
