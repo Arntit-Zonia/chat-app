503744dd5b3201104491eb692bafa3c1b23c3f54
Arntit Zonia <arditzonia@gmail.com>
2023-04-22 20:37:05 +0100

    built the UseUserRoom custom hook

---
+++ client/src/hooks/useUserRoom.ts
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+
+import { UserRoomContext } from "../components/context/UserRoomContext";
+
+/**
+ * Custom hook to access user and room data.
+ * Use this hook within components wrapped by UserRoomProvider.
+ *
+ * @returns An array with user and room data, and a function to update them.
+ * @throws Error if hook is called outside UserRoomProvider.
+ */
+export const useUserRoom = () => {
+  const context = useContext(UserRoomContext);
+
+  if (!context) throw new Error("useUserRoom must be used within a UserRoomProvider");
+
+  return context;
+};
