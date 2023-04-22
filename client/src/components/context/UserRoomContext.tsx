c5369fa914e7b732ce51e536b1d66d10ac6ce05e
Arntit Zonia <arditzonia@gmail.com>
2023-04-22 20:37:38 +0100

    built the user room context

---
+++ client/src/components/context/UserRoomContext.tsx
@@ -0,0 +1,22 @@
+import { createContext, useState, ReactNode, FC } from "react";
+
+interface IUserRoomProviderProps {
+  children: ReactNode;
+}
+
+interface IUserRoom {
+  username: string;
+  room: number;
+}
+
+type TUserRoomContext = [IUserRoom | null, (userRoom: IUserRoom | null) => void];
+
+export const UserRoomContext = createContext<TUserRoomContext | null>(null);
+
+const UserRoomProvider: FC<IUserRoomProviderProps> = ({ children }) => {
+  const [userRoom, setUserRoom] = useState<IUserRoom | null>(null);
+
+  return <UserRoomContext.Provider value={[userRoom, setUserRoom]}>{children}</UserRoomContext.Provider>;
+};
+
+export default UserRoomProvider;
