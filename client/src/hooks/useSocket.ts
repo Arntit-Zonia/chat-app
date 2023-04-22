f491991b8edb152b611585891927457e8a579adc
Arntit Zonia <arditzonia@gmail.com>
2023-04-22 20:36:41 +0100

    added comments to useSocket

--- client/src/hooks/useSocket.ts
+++ client/src/hooks/useSocket.ts
@@ -1,7 +1,12 @@
 import { useContext } from "react";
+import { SocketContext } from "../components/context/SocketContext";
 
-import SocketContext from "../SocketContext";
-
+/**
+ * Custom hook to access the socket instance.
+ * Use this hook within components wrapped by SocketProvider.
+ *
+ * @returns The socket instance from the SocketContext.
+ */
 const useSocket = () => {
   return useContext(SocketContext);
 };
