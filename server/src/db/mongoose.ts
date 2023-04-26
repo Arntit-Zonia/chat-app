d178d6d9af6977a42649b42f3f367b28cbfb2791
Arntit Zonia <arditzonia@gmail.com>
2023-04-26 21:33:55 +0100

    setup mongoose connection

---
+++ server/src/db/mongoose.ts
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectToDatabase = async () => {
+  try {
+    const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/chat";
+
+    await mongoose.connect(MONGO_URI);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+export default connectToDatabase;
--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,9 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 import httpServer from "./app";
+import connectToDatabase from "./db/mongoose";
+
+connectToDatabase();
 
 httpServer.listen(process.env.PORT, () => {
   console.log(`Server started at http://localhost:${process.env.PORT}`);
