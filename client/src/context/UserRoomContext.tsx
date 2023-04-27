a195aaf203c384ab283e18b7a62100a3014d6033
Arntit Zonia <arditzonia@gmail.com>
2023-04-27 22:10:49 +0100

    added the roomData socket event to UserRoomContext

--- client/src/context/UserRoomContext.tsx
+++ client/src/context/UserRoomContext.tsx
@@ -1,12 +1,15 @@
 import { createContext, useState, ReactNode, FC } from "react";
 
+import useSocketEvents from "../hooks/useSocketEvents";
+
 interface IUserRoomProviderProps {
   children: ReactNode;
 }
 
 interface IUserRoom {
-  username: string;
-  room: string;
+  username?: string;
+  room?: string;
+  usernames: string[];
 }
 
 type TUserRoomContext = [IUserRoom | null, (userRoom: IUserRoom | null) => void];
@@ -15,6 +18,20 @@ export const UserRoomContext = createContext<TUserRoomContext | null>(null);
 
 const UserRoomProvider: FC<IUserRoomProviderProps> = ({ children }) => {
   const [userRoom, setUserRoom] = useState<IUserRoom | null>(null);
+
+  useSocketEvents<IUserRoom>([
+    {
+      event: "roomData",
+      callback: ({ usernames }, cb) => {
+        console.log("roomData args", usernames, cb);
+
+        setUserRoom((userRoom) => ({
+          ...userRoom,
+          usernames,
+        }));
+      },
+    },
+  ]);
 
   return <UserRoomContext.Provider value={[userRoom, setUserRoom]}>{children}</UserRoomContext.Provider>;
 };
