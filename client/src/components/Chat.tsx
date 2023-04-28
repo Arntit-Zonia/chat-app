c57568c177f5bfc260ee5e7ae1d87dddb2933e7b
Arntit Zonia <arditzonia@gmail.com>
2023-04-28 21:20:43 +0100

    added scrolling functionality to the Chat component

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { useEffect, useRef, FC } from "react";
 
 import styled from "styled-components";
 
@@ -7,17 +7,41 @@ import useSocketEventsContext from "../hooks/useSocketEventsContext";
 import Message from "./Message";
 import User from "./User";
 import Form from "./GeolocationForm";
+import Sidebar from "./Sidebar";
+
+const ChatLayout = styled.div`
+  display: flex;
+  padding: 20px;
+`;
 
 const StyledChat = styled.ul`
-  width: 80vw;
+  width: 75vw;
+  height: 70vh;
   list-style: none;
   padding: 0;
   margin: 0;
-  height: calc(100% - 150px);
   overflow-y: auto;
   border: 1px solid #ccc;
   border-radius: 4px;
   margin-bottom: 20px;
+
+  /* For Chrome and Edge */
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    background-color: #888;
+    border-radius: 5px;
+  }
+
+  ::-webkit-scrollbar-thumb:hover {
+    background-color: #ccc;
+  }
+
+  /* For Firefox */
+  scrollbar-width: thin;
+  scrollbar-color: #888 #eee;
 `;
 
 const LocationUrl = styled.a`
@@ -29,9 +53,7 @@ const Container = styled.div`
   flex-direction: column;
   align-items: end;
   max-width: 100vw;
-  height: 100vh;
   margin: 0 auto;
-  padding: 20px;
   box-sizing: border-box;
 
   @media (max-width: 768px) {
@@ -42,28 +64,41 @@ const Container = styled.div`
 const Chat: FC = () => {
   const [messages] = useSocketEventsContext();
 
+  const chatRef = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    if (chatRef.current) {
+      const { scrollHeight } = chatRef.current;
+
+      chatRef.current.scrollTop = scrollHeight;
+    }
+  }, [messages]);
+
   return (
-    <Container>
-      <StyledChat>
-        {messages.map(({ username, message, createdAt, isLocation, isNewUser }, i) => (
-          <Message key={i}>
-            {isNewUser ? (
-              <User username={"Admin"} createdAt={createdAt} />
-            ) : (
-              <User username={username} createdAt={createdAt} />
-            )}
-            {isLocation ? (
-              <LocationUrl href={message} target="_blank" rel="noreferrer">
-                My location
-              </LocationUrl>
-            ) : (
-              <>{message}</>
-            )}
-          </Message>
-        ))}
-      </StyledChat>
-      <Form />
-    </Container>
+    <ChatLayout>
+      <Sidebar />
+      <Container>
+        <StyledChat ref={chatRef}>
+          {messages.map(({ username, message, createdAt, isLocation, isNewUser }, i) => (
+            <Message key={i}>
+              {isNewUser ? (
+                <User username={"Admin"} createdAt={createdAt} />
+              ) : (
+                <User username={username} createdAt={createdAt} />
+              )}
+              {isLocation ? (
+                <LocationUrl href={message} target="_blank" rel="noreferrer">
+                  My location
+                </LocationUrl>
+              ) : (
+                <>{message}</>
+              )}
+            </Message>
+          ))}
+        </StyledChat>
+        <Form />
+      </Container>
+    </ChatLayout>
   );
 };
 
