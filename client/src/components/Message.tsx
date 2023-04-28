e5792deb2898fd30de4b26afa4a7a1bf90361a9e
Arntit Zonia <arditzonia@gmail.com>
2023-04-28 21:20:11 +0100

    adjusted Message component margin

--- client/src/components/Message.tsx
+++ client/src/components/Message.tsx
@@ -5,11 +5,11 @@ import styled from "styled-components";
 const StyledMessage = styled.li`
   width: fit-content;
   padding: 12px;
+  margin: 12px;
   border-radius: 5px;
   font-size: 14px;
   color: #e9edef;
   background-color: #075e54;
-  margin-bottom: 5px;
 `;
 
 interface IMessageProps {
