e43cf2e49c8b35b3eb73b1b9fa8016c503b55fa2
Arntit Zonia <arditzonia@gmail.com>
2024-06-01 23:26:55 +0100

    chore: Created .env.example file

--- server/src/db/mongoose.ts
+++ server/src/db/mongoose.ts
@@ -5,6 +5,7 @@ const connectToDatabase = async () => {
     const MONGO_URI = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/chat";
 
     await mongoose.connect(MONGO_URI);
+
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
