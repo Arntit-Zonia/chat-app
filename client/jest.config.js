011cbda255038377606e5312d37e66613be4710a
Arntit Zonia <arditzonia@gmail.com>
2024-06-03 23:14:22 +0100

    fix: implemented import meta error module

--- client/jest.config.js
+++ client/jest.config.js
@@ -6,6 +6,21 @@ module.exports = {
     "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        diagnostics: {
+          ignoreCodes: [1343],
+        },
+        astTransformers: {
+          before: [
+            {
+              path: "node_modules/ts-jest-mock-import-meta",
+              options: { metaObjectReplacement: { url: "https://www.url.com" } },
+            },
+          ],
+        },
+      },
+    ],
   },
 };
